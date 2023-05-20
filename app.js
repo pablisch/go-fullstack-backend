68a269d7562bbdc60457434f31ce2cfe7fb63492
Pablo <pablito@me.com>
2023-05-20 20:13:13 +0100

    completed CRUD operations

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose'); // import mongoose module, to connect to M
 const { password }  = require('./config'); // import the password property of the object exported from config.js
 // the password in config.js is the password for the user pablojoyce, which has read and write access to the database
 
+const Thing = require('./models/thing'); // import the Thing model, which is exported from thing.js
+
 const app = express(); // call the express function, which returns an object with a listen method
 
 mongoose.connect(`mongodb+srv://pablojoyce:${password}@gofullstack.se9tvhr.mongodb.net/?retryWrites=true&w=majority`)
@@ -32,35 +34,48 @@ app.use((req, res, next) => { // call the use method, which adds a middleware fu
 });
 
 app.post('/api/stuff', (req, res, next) => { // call the post method, which adds a route to the app object, to handle POST requests to the /api/stuff endpoint
-  console.log(req.body); // log the request body to the console
-  res.status(201).json({ // set the response status code to 201, and send a response containing the following data
-    message: 'Thing created successfully!' // set the message property of the response data to 'Thing created successfully!'
-  }); // send the response
-}); // call the next function which will be executed in the middleware stack
+  const thing = new Thing({ // create a new Thing object, using the Thing model
+    title: req.body.title, // set the title property of the Thing object to the title property of the request body
+    description: req.body.description, // set the description property of the Thing object to the description property of the request body
+    imageUrl: req.body.imageUrl, // set the imageUrl property of the Thing object to the imageUrl property of the request body
+    price: req.body.price, // set the price property of the Thing object to the price property of the request body
+    userId: req.body.userId, // set the userId property of the Thing object to the userId property of the request body
+  }); // end of const thing = new Thing({ ... })
+  thing.save() // call the save method, which saves the Thing object to the database
+    .then(() => res.status(201).json({ message: 'Thing saved successfully!' })) // call the then method, which adds a callback function to the promise, to handle the success case
+    .catch(error => res.status(400).json({ error: error })); // call the catch method, which adds a callback function to the promise, to handle the failure case
+}); // end of app.post('/api/stuff', (req, res, next) => { ... })  
 
-    
-  
+app.get('/api/stuff/:id', (req, res, next) => { // call the get method, which adds a route to the app object, to handle GET requests to the /api/stuff/:id endpoint
+  Thing.findOne({ _id: req.params.id }) // call the findOne method, which returns a promise, which resolves to the Thing object with the specified id
+    .then(thing => res.status(200).json(thing)) // call the then method, which adds a callback function to the promise, to handle the success case
+    .catch(error => res.status(404).json({ error: error })); // call the catch method, which adds a callback function to the promise, to handle the failure case
+}); // end of app.get('/api/stuff/:id', (req, res, next) => { ... })
+
+app.put('/api/stuff/:id', (req, res, next) => { // call the put method, which adds a route to the app object, to handle PUT requests to the /api/stuff/:id endpoint
+  const thing = new Thing({ // create a new Thing object, using the Thing model
+    _id: req.params.id, // set the _id property of the Thing object to the id property of the request parameters
+    title: req.body.title, // set the title property of the Thing object to the title property of the request body
+    description: req.body.description, // set the description property of the Thing object to the description property of the request body
+    imageUrl: req.body.imageUrl, // set the imageUrl property of the Thing object to the imageUrl property of the request body
+    price: req.body.price, // set the price property of the Thing object to the price property of the request body
+    userId: req.body.userId, // set the userId property of the Thing object to the userId property of the request body
+  }); // end of const thing = new Thing({ ... })
+  Thing.updateOne({ _id: req.params.id }, thing) // call the updateOne method, which returns a promise, which resolves to the Thing object with the specified id
+    .then(() => res.status(201).json({ message: 'Thing updated successfully!' })) // call the then method, which adds a callback function to the promise, to handle the success case
+    .catch(error => res.status(400).json({ error: error })); // call the catch method, which adds a callback function to the promise, to handle the failure case
+}); // end of app.put('/api/stuff/:id', (req, res, next) => { ... })
+
+app.delete('/api/stuff/:id', (req, res, next) => { // call the delete method, which adds a route to the app object, to handle DELETE requests to the /api/stuff/:id endpoint
+  Thing.deleteOne({ _id: req.params.id }) // call the deleteOne method, which returns a promise, which resolves to the Thing object with the specified id
+    .then(() => res.status(200).json({ message: 'Deleted!' })) // call the then method, which adds a callback function to the promise, to handle the success case
+    .catch(error => res.status(400).json({ error: error })); // call the catch method, which adds a callback function to the promise, to handle the failure case
+}); // end of app.delete('/api/stuff/:id', (req, res, next) => { ... })
 
 app.get('/api/stuff', (req, res, next) => { // call the get method, which adds a route to the app object, to handle GET requests to the /api/stuff endpoint
-  const stuff = [
-    {
-      _id: 'oeihfzeoi',
-      title: 'My first thing',
-      description: 'All of the info about my first thing',
-      imageUrl: '',
-      price: 4900,
-      userId: 'qsomihvqios',
-    },
-    {
-      _id: 'oeihfzeomoihi',
-      title: 'My second thing',
-      description: 'All of the info about my second thing',
-      imageUrl: '',
-      price: 2900,
-      userId: 'qsomihvqios',
-    },
-  ];
-  res.status(200).json(stuff);
-});
+  Thing.find() // call the find method, which returns a promise, which resolves to an array of all the Thing objects in the database
+    .then(things => res.status(200).json(things)) // call the then method, which adds a callback function to the promise, to handle the success case
+    .catch(error => res.status(400).json({ error: error })); // call the catch method, which adds a callback function to the promise, to handle the failure case
+}); // end of app.get('/api/stuff', (req, res, next) => { ... })
 
 module.exports = app; // export the app object, so it can be used by other code, e.g. our tests
