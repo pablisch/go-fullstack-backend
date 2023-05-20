13f0a5325f882610117193eda933a5dd0e2ccc01
Pablo <pablito@me.com>
2023-05-20 11:02:14 +0100

    app and server set up

--- app.js
+++ app.js
@@ -2,24 +2,42 @@ const express = require('express'); // import express module, to make our app an
 
 const app = express(); // call the express function, which returns an object with a listen method
 
-
-
-app.use((req, res, next) => {
-  console.log('Request received'); // log a message to the console
-  next(); // call the next method, to pass the request to the next middleware function in the stack
-}); // call the use method, which adds a middleware function to the middleware stack
-
+// The following code is executed for every incoming request
+// The three res.setHeader() methods set the response headers which allow the client to access the API and deal with the CORS policy. Cross Origin Resource Sharing (CORS) is a security mechanism which restricts HTTP requests made from scripts to resources in a different origin. An origin is a combination of a protocol, a domain name and a port number. For example, http://localhost:3000 is an origin. The CORS policy is enforced by the browser. The browser sends an OPTIONS request to the server, to check if the client is allowed to access the API. The server responds with the Access-Control-Allow-Origin header, which tells the browser if the client is allowed to access the API. If the client is allowed to access the API, the browser sends the actual request. The server responds with the requested data. If the client is not allowed to access the API, the browser does not send the actual request. Instead, it sends an error message to the console.
 app.use((req, res, next) => { // call the use method, which adds a middleware function to the middleware stack
-  res.status(201); // set the status code of the response
-  next(); // call the next method, to pass the request to the next middleware function in the stack
+  // set the response header to allow all origins
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  // set the response header to allow the following headers
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); 
+  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // set the response header to allow the following methods
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  // call the next function which will be executed in the middleware stack
+  next();
 }); 
 
-app.use((req, res, next) => { // call the use method, which adds a middleware function to the middleware stack
-  res.json({ message: 'Your request was successful!' }); // send a response with a json object
-}); // call the use method, which adds a middleware function to the middleware stack
+app.use('/api/stuff', (req, res, next) => { // call the use method, which adds a middleware function to the middleware stack
+  const stuff = [
+    {
+      _id: 'oeihfzeoi',
+      title: 'My first thing',
+      description: 'All of the info about my first thing',
+      imageUrl: '',
+      price: 4900,
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoihi',
+      title: 'My second thing',
+      description: 'All of the info about my second thing',
+      imageUrl: '',
+      price: 2900,
+      userId: 'qsomihvqios',
+    },
+  ];
+  res.status(200).json(stuff);
+});
 
-app.use((req, res, next) => { // call the use method, which adds a middleware function to the middleware stack
-  console.log('Response sent successfully'); // log a message to the console
-}); // call the use method, which adds a middleware function to the middleware stack
+
+  
 
 module.exports = app; // export the app object, so it can be used by other code, e.g. our tests
